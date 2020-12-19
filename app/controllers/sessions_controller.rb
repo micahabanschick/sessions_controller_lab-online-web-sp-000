6267f9915764afb9d685dcd46eabfb8171e06ba2
Micah Banschick <micha.ban@gmail.com>
2020-12-19 20:51:13 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    name = params[:name]
+    if session[:name]
+      redirect_to "/"
+    elsif name && name != ""
+      session[:name] = name
+    else
+      render new
+    end
   end
 
   def destroy
