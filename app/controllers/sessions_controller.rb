f93dc06850bbb8218d1effe1b85407abe0b37a81
Micah Banschick <micha.ban@gmail.com>
2020-12-19 20:59:09 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    name = params[:name]
+    if session[:name]
+      redirect_to "/"
+    elsif name && name != ""
+      session[:name] = name
+      redirect_to "/"
+    else
+      redirect_to "/login"
+    end
+  end
+
+  def destroy
+    if session[:name]
+      session[:name] = nil
+    end
+  end
+end
